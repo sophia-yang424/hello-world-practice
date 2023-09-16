51c66212a08511dd038e2d55366af3347087ef1a
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 04:42:11 -0400

    Create handPredictor.py

---
+++ handPredictor.py
@@ -0,0 +1,33 @@
+# imports 
+import numpy as np
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers import Input, Concatenate, Dense, Reshape
+
+# counter for num of data recorded
+from pokerEnv import count
+if count!=0 and count%100==0:
+    
+    fiveCards = np.load(fiveCards)[count-101:]
+    ratioToPot = np.load(ratioToPot)[count-101:]
+    ratioToChip = np.load(ratioToChip)[count-101:]
+    twoCards = np.load(twoCards)[count-101:]
+
+    model = Sequential([
+        input1 = tf.keras.layers.Input(shape=(1,)),
+        input2 = tf.keras.layers.Input(shape=(1,)),
+        input3 = tf.keras.layers.Input(shape=(1,)),
+        merged = tf.keras.layers.Concatenate(axis=1)([input1, input2, input3]).
+        Dense(128, input_dim=3, activation='softmax', use_bias=True)(merged),
+        Dense(128, dropout=0.2, recurrent_dropout=0.2),
+        Dense(),
+    ])
+
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit([fiveCards, ratioToPot, ratioToChip], twoCards, epochs=100, batch_size=32)
+
+    model.save('Directory/handPredictor.h5')
+    model.save_weights('/kaggle/working/weights.h5')
+
