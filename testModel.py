0477d1228184edcb8c4ef3fcc21976297a30fab9
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 16:58:45 -0400

    Create testModel.py

---
+++ testModel.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from keras.models import load_model
+
+model = load_model('handPredictor.h5', compile=False)
+model.compile(loss='sigmoid', optimizer='adam', metrics=['accuracy'])
+
+test1 = [[11,8,42,35,51]]
+test2 = [0.373]
+test3 = [43]
+test1 = np.array(test1)
+test2 = np.array(test2)
+test3 = np.array(test3)
+test = [test1, test2, test3]
+
+result = model.predict(test)
+print(result * 51 + 1)
+
