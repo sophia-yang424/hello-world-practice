c234bff8d2d6a3e6ec48481749b10edf2feb7a53
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 12:03:45 -0400

    Update handPredictor.py

--- handPredictor.py
+++ handPredictor.py
@@ -1,34 +1,34 @@
-# imports 
+# imports  
 import numpy as np
 import tensorflow as tf
 
-from keras.models import Sequential
-from keras.layers import Input, Concatenate, Dense, Reshape
+from keras.models import Model
+from keras.layers import Input, Concatenate, Dense, Dropout
 
-# counter for num of data recorded
-from pokerEnv import count
-if count!=0 and count%100==0:
-    
-    fiveCards = np.load(fiveCards)
-    ratioToPot = np.load(ratioToPot)
-    ratioToChip = np.load(ratioToChip)
-    twoCards = np.load(twoCards)
+#from pokerEnv import count
+def predHandGen(count):
+    if count!=0 and count%100==0:
+        
+        fiveCards = np.load(fiveCards)
+        ratioToPot = np.load(ratioToPot)
+        ratioToChip = np.load(ratioToChip)
+        twoCards = np.load(twoCards)
+        
+        input1 = Input(shape=(5,))
+        input2 = Input(shape=(1,))
+        input3 = Input(shape=(1,))
+        merged = Concatenate(axis=-1)([input1, input2, input3])        
 
-    model = Sequential([
-        input1 = tf.keras.layers.Input(shape=(5,)),
-        input2 = tf.keras.layers.Input(shape=(1,)),
-        input3 = tf.keras.layers.Input(shape=(1,)),
-        merged = tf.keras.layers.Concatenate(axis=-1)([input1, input2, input3]).
-        Dense(128, input_dim=3, activation='softmax', use_bias=True)(merged),
-        Dense(128, dropout=0.2, recurrent_dropout=0.2),
-        Dense(128, dropout=0.2, recurrent_dropout=0.2),
-        Dense(2),
-    ])
+        dense1 = Dense(128, activation='relu')(merged)
+        dropout1 = Dropout(0.2)(dense1)
+        dense2 = Dense(128, activation='relu')(dropout1)
+        dropout2 = Dropout(0.2)(dense2)
+        output = Dense(2, activation='softmax')(dropout2)
 
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model = Model(inputs=[input1, input2, input3], outputs=output)
 
-    model.fit([fiveCards, ratioToPot, ratioToChip], twoCards, epochs=100, batch_size=32)
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-    model.save('Directory/handPredictor.h5')
-    model.save_weights('Directory/weights.h5')
+        model.fit([fiveCards, ratioToPot, ratioToChip], twoCards, epochs=10, batch_size=32)
 
+        model.save('Directory/handPredictor.h5')
