07227dfc95b0c145b5087997c41f127f50c4817c
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 16:58:42 -0400

    Update handPredictor.py

--- handPredictor.py
+++ handPredictor.py
@@ -9,10 +9,19 @@ from keras.layers import Input, Concatenate, Dense, Dropout
 def predHandGen(count):
     if count!=0 and count%100==0:
         
+        '''
         fiveCards = np.load(fiveCards)
         ratioToPot = np.load(ratioToPot)
         ratioToChip = np.load(ratioToChip)
         twoCards = np.load(twoCards)
+        '''
+
+        fiveCards = np.array(fiveCards)
+        ratioToPot = np.array(ratioToPot)
+        ratioToChip = np.array(ratioToChip)
+
+        twoCards = np.array(twoCards)
+        twoCards = (twoCards - 1) / 51
         
         input1 = Input(shape=(5,))
         input2 = Input(shape=(1,))
@@ -23,12 +32,15 @@ def predHandGen(count):
         dropout1 = Dropout(0.2)(dense1)
         dense2 = Dense(128, activation='relu')(dropout1)
         dropout2 = Dropout(0.2)(dense2)
-        output = Dense(2, activation='softmax')(dropout2)
+        output = Dense(2, activation='sigmoid')(dropout2)
 
         model = Model(inputs=[input1, input2, input3], outputs=output)
+        
+        model.summary
 
         model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-        model.fit([fiveCards, ratioToPot, ratioToChip], twoCards, epochs=10, batch_size=32)
+        model.fit([fiveCards, ratioToPot, ratioToChip], twoCards, epochs=100, batch_size=32)
 
-        model.save('Directory/handPredictor.h5')
+        model.save('/Directory/handPredictor.h5')
+
