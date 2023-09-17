c1261f33bd4c8bfb73763331577640490a89804d
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 22:49:47 -0400

    Update testModel.py

--- testModel.py
+++ testModel.py
@@ -7,7 +7,7 @@ model.compile(loss='sigmoid', optimizer='adam', metrics=['accuracy'])
 
 test1 = [[11,8,42,35,51]]
 test2 = [0.373]
-test3 = [43]
+test3 = [0.564]
 test1 = np.array(test1)
 test2 = np.array(test2)
 test3 = np.array(test3)
