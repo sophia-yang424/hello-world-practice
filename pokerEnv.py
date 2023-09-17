8e113e096f90d9058d24086306eafcdc0be8e17d
Keisuke54 <keinakamura5420@gmail.com>
2023-09-16 21:52:51 -0400

    Update pokerEnv.py

--- pokerEnv.py
+++ pokerEnv.py
@@ -1,12 +1,64 @@
-from library import dicts, createDeck
+from library import dicts, deck, fiveCard
+from library import createDeck, shuffelDeck, deal, openCard
 
-# deck
-deck = []
+# whether to continue the game
+cont = True
 
 # hands of players
 p1Hand = []
 p2Hand = []
 
-print("Game starting!")
-createDeck()
+# data collected in the following arrays
+fiveCards = []
+ratioToPot = []
+ratioToChip = []
+
+while cont == True:
+    # initiating game 
+    createDeck()
+    shuffelDeck()
+
+    print("Game starting!")
+
+    # dealing first two hands 
+    deal(p1Hand)
+    deal(p2Hand)
+
+    # betting
+
+    for x in range(3):
+        openCard(x)
+
+    # betting
+
+    openCard(3)
+
+    # betting
+
+    openCard(4)
+
+    # betting
+
+    # print("Who is the winner?")
+    # or
+    # ranking 
+
+    passed = False
+
+    while passed == False:
+        print("Do you want to conitnue? <yes/no>")
+        response = input()
+        response = response.lower()
+
+        if response == "no":
+            cont = False
+            passed = True
+        elif response == "yes":
+            passed = True
+        else:
+            print("Invalid input")
+
+
 print(deck)
+print(len(deck))
+print(openCard)
